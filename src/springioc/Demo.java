fe51468247156ec0411cb10821aa6669f766f664
sayed <sayed@sayed-Latitude-3540>
2014-10-28 09:15:15 +0530

    Demo for file added to show loading mutilple xml file loading

---
+++ src/springioc/Demo.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package springioc;
+
+/**
+ * @author sayed
+ *
+ */
+public class Demo {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Demo [name=" + name + "]";
+	}
+
+}
